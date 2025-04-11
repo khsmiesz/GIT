1fe77ea2dc69901622ea3aab633cbadb5bc662aa
Kasia Śmieszek <khsmiesz@gmail.com>
2025-04-11 21:33:51 +0200

    Add return

--- palindrome.py
+++ palindrome.py
@@ -1,17 +1,20 @@
 #zadanie_palindrom
 
 def is_it_palindrom(word):
+    """
+    Checks if argument is a palindrome by comparing letters in a word forward and backwards
+    There is 1 string argument
+    Return is True if the word is palindrome, False if not
+    """
     forw=[]
     backw=[]
     for letter in word:
         forw += letter 
         backw=forw[::-1]
-    print(forw)
-    print(backw)
-    if forw=backw
-    return 
+    return print(bool(backw==forw))
 is_it_palindrom("baba")
 is_it_palindrom("kajak")
 is_it_palindrom("mamona")
+
 
 
