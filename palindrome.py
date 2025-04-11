54f176f56cc3d75bf1691b3d3af1b9377b0dc306
Kasia Śmieszek <khsmiesz@gmail.com>
2025-04-11 21:18:55 +0200

    Add file

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+#zadanie_palindrom
+
+def is_it_palindrom(word):
+    forw=[]
+    backw=[]
+    for letter in word:
+        forw += letter 
+        backw=forw[::-1]
+    print(forw)
+    print(backw)
+    if forw=backw
+    return 
+is_it_palindrom("baba")
+is_it_palindrom("kajak")
+is_it_palindrom("mamona")
+
+
